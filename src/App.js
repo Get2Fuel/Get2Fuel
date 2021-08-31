ccf77e9d14fc7c1a2793d63d44c0476b3e863f54
gen7 <giusegentilman@gmail.com>
2021-08-31 11:30:36 +0200

    Not working
    Aggiunto proxy che fotte il cors. Api restituiscono oggetto vuoto

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 import React, { useEffect } from "react";
 
+
+var details = {
+  "pointsListStr": "45.70846176147461-9.313352584838867#",
+  "carb": "1-0",
+  "ordPrice": "asc"
+}
+var formBody = [];
+for (var property in details) {
+  var encodedKey = encodeURIComponent(property);
+  var encodedValue = encodeURIComponent(details[property]);
+  formBody.push(encodedKey + "=" + encodedValue);
+}
+formBody = formBody.join("&");
+
 const App = () => {
+   useEffect(async ()=>{
+    const response = await fetch("https://fathomless-chamber-48058.herokuapp.com/https://carburanti.mise.gov.it/OssPrezziSearch/ricerca/position",{
+      method:'POST',
+      headers:{
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Access-Control-Allow-Origin':'*',
+        'Access-Control-Allow-Headers' : 'true',
+        'Access-Control-Allow-Methods' : 'true',
+        'Access-Control-Allow-Credentials': 'false'
+      },
+      body:{ 
+        formBody
+      }
+    });
+    const data = await response.json();
+    console.log(data);
+    
+    //const [stations] = data.results;
+    //console.log(data.results);
+
+  }, []);
   return (
     <div className="App">
-      <p>App</p>
+      <p>wasd</p>
     </div>
   );
 };
 
 export default App;
+
