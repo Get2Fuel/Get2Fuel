1c3194a324e1693d7d0086366fddb8505fc794bf
gen7 <giusegentilman@gmail.com>
2021-09-05 16:19:36 +0200

    Commit del weekend
    Fetcha in maniera corretta ma aggiorna molte volte la posizione(e di conseguenza fa molte richieste alle API). Problema di "hw" gps o sono incapace a usare react?

--- src/App.js
+++ src/App.js
@@ -1,21 +1,57 @@
 import React, { useState } from "react";
-import ReactMapGL, { Marker, GeolocateControl } from 'react-map-gl';
-
-import ApiCall from "./ApiCall";
+import ReactMapGL, { GeolocateControl } from 'react-map-gl';
+import axios from "axios";
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 const App = (() => {
+  //promise che serve per far si che ho subito le coordinate giuste
+  async function getLocation() {
+    return new Promise((res, rej) => {
+      navigator.geolocation.getCurrentPosition(posi => {
+        res(updatePosition(posi));
+      }, rej)
+    })
+  }
+  //funzione che costruisce l'url (non dovrebbe più servire col server express)
+  const buildURL = (details) => {
+    var formBody = [];
+    for (var property in details) {
+      var encodedKey = encodeURIComponent(property);
+      var encodedValue = encodeURIComponent(details[property]);
+      formBody.push(encodedKey + "=" + encodedValue);
+    }
+    formBody = formBody.join("&");
+    const str = "https://fathomless-chamber-48058.herokuapp.com/https://carburanti.mise.gov.it/OssPrezziSearch/ricerca/position?" + formBody;
+    return str;
+  }
+  const [position, setPosition] = useState(0);
+
+  const updatePosition = () => {
+    setPosition(navigator.geolocation.getCurrentPosition((pos) => {
+      /******************************************************************************* */
+      console.log(pos.coords.latitude);
+      console.log(pos.coords.longitude);
+      /******************************************************************************* */
+      var details = {
+        "pointsListStr": pos.coords.latitude + "-" + pos.coords.longitude,
+        "carb": "1-0",
+      }
+      const url = buildURL(details);
+
+      axios.post(url)
+        .then(response => console.log(response.data))
+        .catch(e => console.error(e));
+    }));
+
+  }
+  //parametri per la mappa
   const [viewport, setViewport] = useState({
-    latitude: 45.4211,
-    longitude: -75.6903,
+    latitude: getLocation().latitude,
+    longitude: getLocation().longitude,
     width: "100vw",
     height: "100vh",
     zoom: 10
   });
-  const [position, setPosition] = useState(0);
-
-  const updatePosition =()=>{
-    setPosition(navigator.geolocation.getCurrentPosition((pos) => { const crd = pos.coords; console.log(pos.coords.latitude);console.log(pos.coords.longitude); }));
-  }
 
   return (
     <div>
@@ -30,24 +66,13 @@ const App = (() => {
           positionOptions={{ enableHighAccuracy: true }}
           trackUserLocation={true}
           auto
-          onGeolocate={(position) => { updatePosition (position)}}
+          onGeolocate={(position) => { updatePosition(position) }}
         />
 
-        {/* markers here */}
+        {/* markers */}
       </ReactMapGL>
-      <ApiCall props={position} />
     </div>
 
   )
 });
 export default App;
-
-
-
-
-
-
-// 'Access-Control-Allow-Origin':'*',
-// 'Access-Control-Allow-Headers' : 'true',
-// 'Access-Control-Allow-Methods' : 'true',
-// 'Access-Control-Allow-Credentials': 'false'
