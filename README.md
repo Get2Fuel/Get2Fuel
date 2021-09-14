522b5995efc8fdaebff873e445bb9e403b6859ef
Lorenzo Andreotti <lorenzo.andreotti.2000@gmail.com>
2021-09-14 21:46:01 +0200

    Fixed API call
    - now the API call passes through the connector for openAPI available at https://github.com/Get2Fuel/OsservaPrezzi-Connector
    - updated README.md
    - minor changes

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Get2Fuel
+
+---
+
+## Commits history
+
+### 2021-09-14
+
+#### Fixed API call
+
+- now the API call passes through the connector for openAPI available at https://github.com/Get2Fuel/OsservaPrezzi-Connector
+- updated README.md
+- minor changes
+
+### 2021-09-14
+
+#### Working API
+
+Added support for
+
+- get pumps via manual search
+- get pumps via coordinates
+
+### 2021-08-30
+
+#### Initial commit
--- src/App.js
+++ src/App.js
@@ -1,78 +1,56 @@
 import React, { useState } from "react";
-import ReactMapGL, { GeolocateControl } from 'react-map-gl';
+import ReactMapGL, { GeolocateControl } from "react-map-gl";
 import axios from "axios";
-import 'mapbox-gl/dist/mapbox-gl.css';
+import "mapbox-gl/dist/mapbox-gl.css";
 
-const App = (() => {
-  //promise che serve per far si che ho subito le coordinate giuste
-  async function getLocation() {
-    return new Promise((res, rej) => {
-      navigator.geolocation.getCurrentPosition(posi => {
-        res(updatePosition(posi));
-      }, rej)
-    })
-  }
-  //funzione che costruisce l'url (non dovrebbe più servire col server express)
-  const buildURL = (details) => {
-    var formBody = [];
-    for (var property in details) {
-      var encodedKey = encodeURIComponent(property);
-      var encodedValue = encodeURIComponent(details[property]);
-      formBody.push(encodedKey + "=" + encodedValue);
-    }
-    formBody = formBody.join("&");
-    const str = "https://fathomless-chamber-48058.herokuapp.com/https://carburanti.mise.gov.it/OssPrezziSearch/ricerca/position?" + formBody;
-    return str;
-  }
+const App = () => {
   const [position, setPosition] = useState(0);
 
   const updatePosition = () => {
-    setPosition(navigator.geolocation.getCurrentPosition((pos) => {
-      /******************************************************************************* */
-      console.log(pos.coords.latitude);
-      console.log(pos.coords.longitude);
-      /******************************************************************************* */
-      var details = {
-        "pointsListStr": pos.coords.latitude + "-" + pos.coords.longitude,
-        "carb": "1-0",
-      }
-      const url = buildURL(details);
+    setPosition(
+      navigator.geolocation.getCurrentPosition((pos) => {
+        console.log(pos.coords.latitude);
+        console.log(pos.coords.longitude);
+        axios
+          .get(
+            `http://localhost:3100/geolocate/${pos.coords.latitude}/${pos.coords.longitude}/gasoline/any/`
+          )
+          .then((response) => console.log(response.data))
+          .catch((e) => console.error(e));
+      })
+    );
+  };
 
-      axios.post(url)
-        .then(response => console.log(response.data))
-        .catch(e => console.error(e));
-    }));
-
-  }
-  //parametri per la mappa
   const [viewport, setViewport] = useState({
-    latitude: getLocation().latitude,
-    longitude: getLocation().longitude,
+    latitude: navigator.geolocation.getCurrentPosition.latitude,
+    longitude: navigator.geolocation.getCurrentPosition.longitude,
     width: "100vw",
     height: "100vh",
-    zoom: 10
+    zoom: 10,
   });
 
   return (
-    <div>
-      <ReactMapGL
-        {...viewport}
-        mapboxApiAccessToken={"pk.eyJ1IjoibG9yZW56b2FuZHJlb3R0aSIsImEiOiJja3Q0ZndxYmoweHVrMzByMWdrNHd5Zno1In0.JXPUPxIQ6yLbD3HBa_pHvQ"}
-        onViewportChange={viewport => {
-          setViewport(viewport);
+    <ReactMapGL
+      {...viewport}
+      mapboxApiAccessToken={
+        "pk.eyJ1IjoibG9yZW56b2FuZHJlb3R0aSIsImEiOiJja3Q0ZndxYmoweHVrMzByMWdrNHd5Zno1In0.JXPUPxIQ6yLbD3HBa_pHvQ"
+      }
+      mapStyle={"mapbox://styles/mapbox/dark-v10"}
+      onViewportChange={(viewport) => {
+        setViewport(viewport);
+      }}
+    >
+      <GeolocateControl
+        positionOptions={{ enableHighAccuracy: true }}
+        trackUserLocation={true}
+        auto
+        onGeolocate={(position) => {
+          updatePosition(position);
         }}
-      >
-        <GeolocateControl
-          positionOptions={{ enableHighAccuracy: true }}
-          trackUserLocation={true}
-          auto
-          onGeolocate={(position) => { updatePosition(position) }}
-        />
+      />
 
-        {/* markers */}
-      </ReactMapGL>
-    </div>
-
-  )
-});
+      {/* markers */}
+    </ReactMapGL>
+  );
+};
 export default App;
